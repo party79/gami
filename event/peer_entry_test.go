b0e73d667abaa57bfa2d2b99b21004ffc9a07f03
Martyn Allan <martyn.allan@ladbrokes.com.au>
2016-09-02 16:15:50 +1000

    Adding missing tests for PeerEntry

---
+++ event/peer_entry_test.go
@@ -0,0 +1,37 @@
+package event
+
+import (
+	"testing"
+
+	"github.com/bit4bit/gami"
+)
+
+func TestPeerEntry(t *testing.T) {
+	fixture := map[string]string{
+		"Channeltype":    "Channeltype",
+		"Objectname":     "Objectname",
+		"Chanobjecttype": "Chanobjecttype",
+		"Ipaddress":      "Ipaddress",
+		"Ipport":         "Ipport",
+		"Dynamic":        "Dynamic",
+		"Natsupport":     "Natsupport",
+		"Videosupport":   "Videosupport",
+		"Textsupport":    "Textsupport",
+		"Acl":            "Acl",
+		"Status":         "Status",
+		"Realtimedevice": "Realtimedevice",
+	}
+
+	ev := gami.AMIEvent{
+		ID:        "PeerEntry",
+		Privilege: []string{"all"},
+		Params:    fixture,
+	}
+
+	evtype := New(&ev)
+	if _, ok := evtype.(*PeerEntry); !ok {
+		t.Fatal("PeerEntry type assertion")
+	}
+
+	testEvent(t, fixture, evtype)
+}
